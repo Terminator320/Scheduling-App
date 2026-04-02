ee8b3fb1bc00e33fb99b3c18464911af18e087de
terminator320 <george@vogas.net>
2026-04-02 00:34:32 -0400

    added test splash screen and added base themes for dark/light mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:scheduling/utils/themes.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'firebase_options.dart';
 
 import 'views/login.dart';
@@ -15,21 +17,70 @@ void main() async {
     options: DefaultFirebaseOptions.android,
   );
 
-  runApp(MyApp());
+  runApp(PaulApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PaulApp extends StatefulWidget {
+  const PaulApp({super.key});
+
+  @override
+  State<PaulApp> createState() => _PaulAppState();
+}
+
+class _PaulAppState extends State<PaulApp> {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  void toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.dark
+          ? ThemeMode.light
+          : ThemeMode.dark;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Scheduling App',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/' : (context) => Login(),
-      },
+      theme: Themes().lightTheme,
+      darkTheme: Themes().darkTheme,
+      themeMode: _themeMode,
+      home: Splash(toggleTheme: toggleTheme),
+    );
+  }
+}
+
+
+class Splash extends StatefulWidget {
+  final VoidCallback toggleTheme;
+
+  const Splash({super.key, required this.toggleTheme});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 10,
+      navigateAfterSeconds: Login(),
+      title: Text(
+        'Welcome to my Product',
+        style: Theme.of(context).textTheme.headlineMedium,
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      styleTextUnderTheLoader: TextStyle(),
+      image: Image.asset('assets/george.JPG'),
+      photoSize: 150,
+      loaderColor: Theme.of(context).colorScheme.primary,
+      loadingText: Text(
+        'Hey Welcome ',
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+      loadingTextPadding: EdgeInsets.zero,
+      useLoader: true,
     );
   }
 }
